908b622b96fa1678d28e945d9e0d49bf82aab9a2
adrianrodriguez <ardgz2003@gmail.com>
2026-05-27 22:59:42 -0500

    Created a constructor and getters for the Appetizer.java

--- src/main/java/com/pluralsight/item/Appetizer.java
+++ src/main/java/com/pluralsight/item/Appetizer.java
@@ -2,8 +2,21 @@ package com.pluralsight.item;
 
 public class Appetizer {
 
+
+    //Create a tiny constructor for the appetizers
+    private String dipping;
+    private double price;
+
+    public Appetizer(String dipping, double price) {
+        this.dipping = dipping;
+        this.price = price;
+    }
+
+    public String getDipping() {
+        return dipping;
+    }
+
     public double getPrice() {
         return price;
     }
-
 }
