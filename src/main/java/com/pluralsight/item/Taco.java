98155883838f52e09c95b415875d17413cb4ba57
adrianrodriguez <ardgz2003@gmail.com>
2026-05-28 23:38:48 -0500

    Built the Taco.java class

--- src/main/java/com/pluralsight/item/Taco.java
+++ src/main/java/com/pluralsight/item/Taco.java
@@ -9,24 +9,116 @@ public class Taco {
     private String tortilla;
     private ArrayList<String> protein;
     private ArrayList<String> dairy;
-    private ArrayList<String> regularToppings;
-    private String salsas;
+    private ArrayList<String> toppings;
+    private String salsa;
     private ArrayList<String> sides;
     private double price;
 
-    public Taco(String size, String tortilla, ArrayList<String> protein, ArrayList<String> dairy, ArrayList<String> regularToppings, String salsas, ArrayList<String> sides, double price) {
+    public Taco(String size, String tortilla, ArrayList<String> protein, ArrayList<String> dairy, ArrayList<String> toppings, String salsas, ArrayList<String> sides, double price) {
         this.size = size;
         this.tortilla = tortilla;
         this.protein = protein;
         this.dairy = dairy;
-        this.regularToppings = regularToppings;
-        this.salsas = salsas;
+        this.toppings = toppings;
+        this.salsa = salsa;
         this.sides = sides;
         this.price = price;
+
+        calculatePrice();
     }
 
+    private void calculatePrice(){
+        price = 0;
+
+        if (size.equalsIgnoreCase("S")) {
+            price += 2.70;
+        } else if (size.equalsIgnoreCase("L")) {
+            price += 3.70;
+        }
+
+        if (protein != null) {
+            for (String p : protein) {
+                if (p.equalsIgnoreCase("Chicken"))
+                    price += 1.45;
+                else if (p.equalsIgnoreCase("Sirloin"))
+                    price += 1.45;
+                else if (p.equalsIgnoreCase("Barbacoa"))
+                    price += 1.45;
+                else if  (p.equalsIgnoreCase("Sausage"))
+                    price += 0.75;
+                else if  (p.equalsIgnoreCase("Chorizo"))
+                    price += 0.75;
+                else if  (p.equalsIgnoreCase("Eggs"))
+                    price += 0.45;
+                else if  (p.equalsIgnoreCase("Bacon"))
+                    price += 0.45;
+                else if  (p.equalsIgnoreCase("Veggie Nut Chorizo"))
+                    price += 1.45;
+                else if  (p.equalsIgnoreCase("Picadillo"))
+                    price += 1.45;
+            }
+        }
+
+        if (dairy != null) {
+            for (String p : dairy) {
+                if (p.equalsIgnoreCase("Jack Cheese"))
+                    price += 0.45;
+                else if (p.equalsIgnoreCase("Queso Fresco"))
+                    price += 0.45;
+                else if  (p.equalsIgnoreCase("Queso Liquido"))
+                    price += 0.45;
+                else if  (p.equalsIgnoreCase("Sour Cream"))
+                    price += 0.45;
+            }
+        }
+
+        if (salsa != null && salsa.equalsIgnoreCase("Dona")) {
+            price += 0.10;
+        }
+
+        if (toppings != null) {
+            price += toppings.size() * 0.45;
+        }
+
+        if (sides != null) {
+            price += sides.size() * 1.75;
+        }
+    }
+
+    public String getSize() {
+        return size;
+    }
+    public String getTortilla() {
+        return tortilla;
+    }
+    public ArrayList<String> getProtein() {
+        return protein;
+    }
+    public ArrayList<String> getDairy() {
+        return dairy;
+    }
+    public ArrayList<String> getToppings() {
+        return toppings;
+    }
+    public String getSalsa() {
+        return salsa;
+    }
+    public ArrayList<String> getSides() {
+        return sides;
+    }
     public double getPrice() {
         return price;
+    }
+
+    @Override
+    public String toString() {
+        return "Taco (" + size + ", " + tortilla +
+                ", Proteins: " + protein +
+                ", Dairy: " + dairy +
+                ", Salsa: " + salsa +
+                ", Toppings: " + toppings +
+                ", Sides: " + sides +
+                ") Price: $" + String.format("%.2f", price);
     }
 
 }
